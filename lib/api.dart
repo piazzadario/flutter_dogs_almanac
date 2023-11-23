b47d09ec5fcea6625803c313881688b4166e2c62
Dario Piazza <dariopiazza007@gmail.com>
2023-11-23 11:59:35 +0100

    feat: getRandomImageBySubBreed

--- lib/api.dart
+++ lib/api.dart
@@ -25,4 +25,17 @@ class Api {
 
     throw Exception('Failed to load $breed images');
   }
+
+  Future<String> getRandomImageBySubBreed({
+    required String breed,
+    required String subBreed,
+  }) async {
+    final response = await http
+        .get(Uri.parse('$_baseUrl/breed/$breed/$subBreed/images/random'));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body)['message'];
+    }
+
+    throw Exception('Failed to load $breed/$subBreed images');
+  }
 }
