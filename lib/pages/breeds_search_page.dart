1dd11754095353e9fb5501c7d03646f254d95a83
Dario Piazza <dariopiazza007@gmail.com>
2023-11-24 12:23:04 +0100

    refactor: breed search page

--- lib/pages/breeds_search_page.dart
+++ lib/pages/breeds_search_page.dart
@@ -1,5 +1,7 @@
 import 'package:dogs_almanac/bloc/dog_breeds_bloc.dart';
-import 'package:dogs_almanac/pages/all_breeds_page.dart';
+import 'package:dogs_almanac/enum/request_status.dart';
+import 'package:dogs_almanac/events/dogs_event.dart';
+import 'package:dogs_almanac/extensions/string_extension.dart';
 import 'package:dogs_almanac/states/dog_breeds_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,60 +19,102 @@ class BreedsSearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<DogBreedsBloc, DogBreedsState>(
-      builder: (context, state) {
-        return Scaffold(
-          appBar: AppBar(
-            leading: GestureDetector(
-              child: const Icon(Icons.arrow_back),
-              onTap: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            title: Text(title),
-            centerTitle: true,
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          ),
-          body: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SearchBar(
-                  controller: _controller,
-                  hintText: "e.g. \"Beagle\"",
-                ),
-              ),
-              Expanded(
-                child: ValueListenableBuilder(
-                  valueListenable: _controller,
-                  builder: (context, value, child) {
-                    final filteredBreeds = state.breeds.where((breed) =>
-                        breed.name.toLowerCase().contains(value.text));
+    return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+          child: const Icon(Icons.arrow_back),
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Text(title),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: BlocBuilder<DogBreedsBloc, DogBreedsState>(
+        builder: (context, state) {
+          switch (state.status) {
+            case RequestStatus.initial:
+            case RequestStatus.pending:
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            case RequestStatus.failed:
+              return _BreedsSearchError();
+            case RequestStatus.success:
+              return Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SearchBar(
+                      controller: _controller,
+                      hintText: "e.g. \"Beagle\"",
+                    ),
+                  ),
+                  Expanded(
+                    child: ValueListenableBuilder(
+                      valueListenable: _controller,
+                      builder: (context, value, child) {
+                        final allBreeds = includeSubBreeds
+                            ? state.getAllSubBreeds()
+                            : state.breeds.map((b) => b.name);
 
-                    if (filteredBreeds.isEmpty) {
-                      return _EmptySearchResult(searchTerm: value.text);
-                    }
-                    
-                    return ListView.builder(
-                      padding: const EdgeInsets.all(12),
-                      itemCount: filteredBreeds.length,
-                      itemBuilder: (context, index) {
-                        return BreedCard(filteredBreeds.elementAt(index));
+                        final filteredBreeds = allBreeds.where((breedName) =>
+                            breedName.toLowerCase().contains(value.text));
+
+                        if (filteredBreeds.isEmpty) {
+                          return _EmptySearchResult(searchTerm: value.text);
+                        }
+
+                        return ListView.builder(
+                          padding: const EdgeInsets.all(12),
+                          itemCount: filteredBreeds.length,
+                          itemBuilder: (context, index) {
+                            return _BreedSearchResultCard(
+                                filteredBreeds.elementAt(index));
+                          },
+                        );
                       },
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-        );
-      },
+                    ),
+                  ),
+                ],
+              );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class _BreedsSearchError extends StatelessWidget {
+  const _BreedsSearchError({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text(
+          'An error occurred.\nPlease try again',
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        ElevatedButton.icon(
+          onPressed: () {
+            context.read<DogBreedsBloc>().add(GetBreeds());
+          },
+          icon: const Icon(Icons.replay),
+          label: const Text('Try again'),
+        )
+      ],
     );
   }
 }
 
 class _EmptySearchResult extends StatelessWidget {
   final String searchTerm;
+
   const _EmptySearchResult({
     required this.searchTerm,
     super.key,
@@ -82,6 +126,31 @@ class _EmptySearchResult extends StatelessWidget {
       child: Text(
         'No results for "$searchTerm".\nTry with a different breed.',
         textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
+
+class _BreedSearchResultCard extends StatelessWidget {
+  final String breedName;
+
+  const _BreedSearchResultCard(
+    this.breedName, {
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () {},
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            breedName.capitalized(),
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+        ),
       ),
     );
   }
